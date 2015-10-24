71e4c8b94bbeb777811aa615935f5baebcb9a701
tonirilix <jesusmontes.mx@hotmail.com>
2015-10-24 11:34:26 -0500

    Adding init api

---
+++ server/api/controllers/InitController.js
@@ -0,0 +1,11 @@
+/**
+ * InitController
+ *
+ * @description :: Server-side logic for managing inits
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
