52d9c533a9e69c5e2daa23604533067fbcbacac3
tonirilix <jesusmontes.mx@hotmail.com>
2016-05-01 11:39:19 -0500

    Remove again

--- client/dist/dev/app/+alucin/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require('./components/alucin.component'));
-
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC8rYWx1Y2luL2luZGV4LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7QUFBQSxpQkFBYywrQkFBK0IsQ0FBQyxFQUFBIiwiZmlsZSI6ImFwcC8rYWx1Y2luL2luZGV4LmpzIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0ICogZnJvbSAnLi9jb21wb25lbnRzL2FsdWNpbi5jb21wb25lbnQnO1xuXG4iXX0=
--- client/dist/tmp/app/+alucin/components/alucin.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from 'angular2/core';
-
-@Component({
-  selector: 'sd-alucin',
-  templateUrl: 'app/+alucin/components/alucin.component.html',
-  styleUrls: ['app/+alucin/components/alucin.component.css']
-})
-export class AlucinComponent {}
